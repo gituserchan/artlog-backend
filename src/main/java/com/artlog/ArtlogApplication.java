7ca7e8eef3bae5b96346495690e68c6217e95860
gituserchan <pcj4142@naver.com>
2026-06-02 16:56:52 +0900

    Initial commit

---
+++ src/main/java/com/artlog/ArtlogApplication.java
@@ -0,0 +1,13 @@
+package com.artlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artlog/ArtlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
